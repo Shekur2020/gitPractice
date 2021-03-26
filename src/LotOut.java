ad234c8e0fdd239a5efb1da68520c0c2aa069035
Noorl <ab427902@dal.ca>
2021-03-26 16:35:38 -0400

    git push soon

---
+++ src/LotOut.java
@@ -0,0 +1,7 @@
+public class LotOut {
+
+    public static void main(String[] args) {
+        System.out.println("\"\" +\n            \"Git push\" = " + "" +
+                "Git push");
+    }
+}
