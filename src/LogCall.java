bc916531f880759a4cf0ad7b0866967d5be61ae0
Noorl <ab427902@dal.ca>
2021-03-26 17:33:30 -0400

    add logCall.java

---
+++ src/LogCall.java
@@ -0,0 +1,8 @@
+public class LogCall {
+
+    public static void main(String[] args) {
+
+        String m = "Calling Muhtar";
+        System.out.println("m = " + m);
+    }
+}
--- src/LotOut.java
+++ src/LotOut.java
@@ -3,5 +3,8 @@ public class LotOut {
     public static void main(String[] args) {
         System.out.println("\"\" +\n            \"Git push\" = " + "" +
                 "Git push");
+
+
+
     }
 }
