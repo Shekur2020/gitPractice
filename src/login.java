65d2285a95eb3518e4d7fc054a9dc89225c84629
Noorl <ab427902@dal.ca>
2021-03-26 13:32:43 -0400

    add login.java again

--- src/login.java
+++ src/login.java
@@ -3,5 +3,6 @@ public class login {
     public static void main(String[] args) {
 
         System.out.println("Hello Git");
+        System.out.println("\"Java is fun and git too\" = " + "Java is fun and git too");
     }
 }
