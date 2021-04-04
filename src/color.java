7e2e930c4b1ae4c5e18890aecc717896366d7852
Noorl <ab427902@dal.ca>
2021-04-04 11:43:39 -0400

    commiting just one file

---
+++ src/color.java
@@ -0,0 +1,12 @@
+public class color {
+    public static void main(String[] args) {
+
+        String str1 = "red";
+        String str2 = "green";
+        String str3 = "blue";
+
+
+        System.out.println( str1 + " "+ str2 + " "+  str3);
+    }
+
+}
